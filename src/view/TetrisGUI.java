e7d8b91318035fb4e8bb58c6f746ac04bd28cc48
daltonmilti <dmilti@uw.edu>
2023-11-21 13:41:47 -0800

    Copied create and showGUi from exampleGUI

--- src/view/TetrisGUI.java
+++ src/view/TetrisGUI.java
@@ -1,15 +1,25 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class TetrisGUI {
 
     public static void CreateAndShowGUI() {
+        final TetrisGUI mainPanel =
+                new TetrisGUI();
 
-        JFrame frame = new JFrame();
-        frame.setVisible(true);
+        final Dimension frameSize = new Dimension(400, 400);
 
+        final JFrame window = new JFrame("A Message");
 
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //window.setContentPane(mainPanel);
+
+        window.pack();
+
+        window.setVisible(true);
     }
 }
 
